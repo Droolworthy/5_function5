fcf0423731182005f43c7886918767972394533b
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-09-16 17:49:15 +0300

    Добавьте файлы проекта.

---
+++ 5_function5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _5_function5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+
+            Shuffle(array);
+
+            for(int i = 0; i < array.Length; i++)
+            {     
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadKey();
+        }
+
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = random.Next(i + 1);
+                int value = array[i];
+                array[i] = array[j];
+                array[j] = value;               
+            }
+        }                
+    }
+}
